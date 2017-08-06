b1c6b82f34a27d2ce0bd120edbea42425bdeeded
Ken Snyder <ken@ken.net>
2017-08-05 19:50:17 -0700

    no default export, export minimal API via index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {default as Mock} from './mock';
+export {default as Reference} from './reference';
+export {default as Query} from './query';
+export {default as SnapShot} from './snapshot';
