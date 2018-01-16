1654adc1fb85185ca521e2f6c501aa9990f3d062
Ken Snyder <ken@ken.net>
2018-01-16 13:37:55 -0800

    updated test script

--- scripts/test.ts
+++ scripts/test.ts
@@ -3,53 +3,34 @@ import chalk from "chalk";
 import { exec } from "shelljs";
 import * as rm from "rimraf";
 import * as process from "process";
+import * as program from "commander";
 import "../test/testing/test-console";
 import { stdout, stderr } from "test-console";
 
-function prepOutput(output: string) {
-  return output.replace(/\t\r\n/, "").replace("undefined", "");
-}
+function getScope(files: string): string {
+  let fileScope: string;
 
-function getExecutionStage(): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const inspect = stdout.inspect();
-    exec(`npm get stage`, (code, output) => {
-      inspect.restore();
+  if (!files) {
+    console.log(
+      chalk.white(
+        "no specific files specified so all files being tested, use -h for more help"
+      )
+    );
+    fileScope = "--recursive test/**/*-spec.ts";
+  } else {
+    const prefix = files.slice(0, 5) === "test/" ? "" : "test/";
+    const postfix = files.slice(-5) === "-spec" ? "" : "-spec";
 
-      const result = prepOutput(output).trim();
-      resolve(result ? result : "test");
-    });
-  });
-}
+    fileScope = prefix + files + postfix + ".ts";
+  }
 
-function getScope(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    let fileScope: string;
+  console.log(
+    chalk.green(
+      `${chalk.bold("mocha")} --compilers ts:ts-node/register  ${fileScope}`
+    )
+  );
 
-    exec(`npm get files`, (code, out) => {
-      if (!out || out === "undefined\n") {
-        console.log(
-          chalk.white(
-            'no files specified with "--files=file.ts" option so all files being tested'
-          )
-        );
-        fileScope = "--recursive test/**/*-spec.ts";
-      } else {
-        const prefix = out.slice(0, 5) === "test/" ? "" : "test/";
-        const postfix = out.slice(-5) === "-spec" ? "" : "-spec";
-        out = out.split(".")[0].replace(/\W/, "");
-
-        fileScope = prefix + out + postfix + ".ts";
-      }
-
-      console.log(
-        chalk.green(
-          `${chalk.bold("mocha")} --compilers ts:ts-node/register  ${fileScope}`
-        )
-      );
-      resolve(fileScope);
-    });
-  });
+  return fileScope;
 }
 
 /**
@@ -58,24 +39,42 @@ function getScope(): Promise<string> {
  * may represent unintentional stale tests
  */
 function cleanJSTests() {
-  rm.sync("test/**/*.js");
+  return new Promise((resolve, reject) => {
+    rm("test/**/*.js", e => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
 
 function executeTests(stg: string, fileScope: string): void {
   process.env.AWS_STAGE = stg;
   process.env.TS_NODE_COMPILER_OPTIONS = '{ "noImplicitAny": false }';
-  exec(
-    `mocha --compilers ts:ts-node/register ` +
-      `--compilerOptions --require ts-node/register ` +
-      fileScope
-  );
+  exec(`mocha --require ts-node/register ` + fileScope);
 }
 
-let stage: string;
-let scope: string;
+program
+  .arguments("[files]")
+  .option(
+    "-s, --stage [env]",
+    "Environment to use",
+    /^(dev|test|stage|prod)^/,
+    "test"
+  )
+  .option(
+    "-f, --files",
+    "an alternative syntax to just specifying files as first argument on command line"
+  )
+  .action(async files => {
+    console.log(files, program.stage);
+    await cleanJSTests();
+    const stage = program.stage;
+    const scope = getScope(files);
+    console.log("scope:", scope);
 
-getExecutionStage()
-  .then(stg => Promise.resolve((stage = stg)))
-  .then(() => getScope())
-  .then(sc => Promise.resolve((scope = sc)))
-  .then(() => executeTests(stage, scope));
+    await executeTests(stage, scope);
+  })
+  .parse(process.argv);
