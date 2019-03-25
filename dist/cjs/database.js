e5a7082d7fbb84902e2c1d1a36e81b1b7c6a10de
Ken Snyder <ken@ken.net>
2019-03-24 20:44:41 -0700

    removed full lodash as dep

--- dist/cjs/database.js
+++ dist/cjs/database.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { set, get } from "lodash";
 const lodash_set_1 = __importDefault(require("lodash.set"));
 const lodash_get_1 = __importDefault(require("lodash.get"));
 const firebase_key_1 = require("firebase-key");
