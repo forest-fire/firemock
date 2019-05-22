73ddf8584b5ca5ae000ffe6185d4732b52f35568
Ken Snyder <ken@ken.net>
2019-05-22 09:46:12 -0700

    temp remove error file

--- src/errors/FireMockError.ts
+++
@@ -1,9 +0,0 @@
-export class FireMockError extends Error {
-  public firemodel = true;
-  public code: string;
-  constructor(message: string, name: string = "firemock/error") {
-    super(message);
-    this.name = name;
-    this.code = name.split("/").pop();
-  }
-}
