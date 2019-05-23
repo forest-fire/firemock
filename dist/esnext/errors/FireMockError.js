03d568cb9a98584fa04e12ea04a687a75a6fa6af
Ken Snyder <ken@ken.net>
2019-05-23 11:33:32 -0700

    hopefully not mac nonesense

--- dist/esnext/errors/FireMockError.js
+++
@@ -1,8 +0,0 @@
-export class FireMockError extends Error {
-    constructor(message, name = "firemock/error") {
-        super(message);
-        this.firemodel = true;
-        this.name = name;
-        this.code = name.split("/").pop();
-    }
-}
