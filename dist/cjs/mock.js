84db06bf279f4ba4c8e62b701d97637bde3bf0d7
Ken Snyder <ken@ken.net>
2019-10-04 12:08:21 -0700

    db initialization config will accept IDictionary OR an async function (new)

--- dist/cjs/mock.js
+++ dist/cjs/mock.js
@@ -63,7 +63,11 @@ class Mock {
             validEmailUsers: []
         };
         const defaultDbConfig = {};
-        const obj = new Mock(options.db || defaultDbConfig, options.auth
+        const obj = new Mock(options.db
+            ? typeof options.db === "function"
+                ? await options.db()
+                : options.db || defaultDbConfig
+            : defaultDbConfig, options.auth
             ? Object.assign({}, defaultAuthConfig, options.auth) : defaultAuthConfig);
         await obj.importFakerLibrary();
         return obj;
@@ -75,10 +79,17 @@ class Mock {
         return new index_1.Deployment();
     }
     /**
-     * Update (non-desctructively) the mock DB with a raw JS object/hash
+     * Update -- _non-desctructively_ -- the mock DB with a JS object/hash
      */
-    updateDB(state) {
-        database_1.updateDatabase(state);
+    updateDB(
+    /** the _new_ state that will be updated with the old */
+    stateUpdate, 
+    /** optionally clear the DB before applying the update */
+    clearFirst) {
+        if (clearFirst) {
+            database_1.clearDatabase();
+        }
+        database_1.updateDatabase(stateUpdate);
     }
     /**
      * silences the database from sending events;
