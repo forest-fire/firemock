c398d04b4f2fa7c7b9453c0b1ab992e211cbc094
Ken Snyder <ken@ken.net>
2019-11-19 16:20:13 -0800

    removed provider "instance" classes

--- src/auth/AuthProviders/EmailAuthProvider_Instance.ts
+++
@@ -1,6 +0,0 @@
-import { AuthProvider } from "@firebase/auth-types";
-
-// tslint:disable-next-line: class-name
-export class EmailAuthProvider_Instance implements AuthProvider {
-  public providerId: string;
-}
